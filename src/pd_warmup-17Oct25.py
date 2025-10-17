2e8d4be1258d7c10eaf6d4b143f8ba3f3b8c64e9
Sean McLeaish <smcleaish@gmail.com>
2025-10-17 13:05:26 -0400

    updated to return just the value in pd_warmup

--- src/pd_warmup-17Oct25.py
+++ src/pd_warmup-17Oct25.py
@@ -1,25 +1,26 @@
 from dataclasses import dataclass
 import sys
+from typing import Hashable
 
 import pandas as pd
 
 
-@dataclass
+@dataclass(frozen=True, slots=True)
 class DataClean:
     df: pd.DataFrame
 
-    def max_by(self, id: str, val: str) -> pd.Series:
-        return self.df[[id, val]].max()
+    def max_by(self, id: str, val: str) -> Hashable:
+        return self.df[[id, val]].max().reset_index().iloc[0, 1]
 
-    def min_by(self, id: str, val: str) -> pd.Series:
-        return self.df[[id, val]].min()
+    def min_by(self, id: str, val: str) -> Hashable:
+        return self.df[[id, val]].min().reset_index().iloc[0, 1]
 
-    def gt(self, id: str, val: str) -> pd.Series:
-        return self.df[id][self.df[val] > 200]
+    def gt(self, id: str, val: str, amt: int) -> str:
+        return ", ".join(list(self.df[id][self.df[val] > amt]))
 
 
 if __name__ == "__main__":
     aggdata = DataClean(df=pd.read_csv(sys.argv[1]))
-    print(f"Highest exports: {aggdata.max_by('state', 'total exports')}")
-    print(f"Lowest pork exports: {aggdata.min_by('state', 'pork')}")
-    print(f"Dairy over 200: {aggdata.gt('state', 'dairy')}")
+    print(f"Highest exports: \n{aggdata.max_by('state', 'total exports')}\n")
+    print(f"Lowest pork exports: \n{aggdata.min_by('state', 'pork')}\n")
+    print(f"Dairy over 200: \n{aggdata.gt('state', 'dairy', 200)}\n")
