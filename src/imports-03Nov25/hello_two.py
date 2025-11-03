6cb0f1b24ba7684da2647ff51e93fb8ae9d21a88
Sean McLeaish <smcleaish@gmail.com>
2025-11-03 16:32:59 -0500

    import exercise

---
+++ src/imports-03Nov25/hello_two.py
@@ -0,0 +1,6 @@
+def hello_two() -> None:
+    print('I can also say hello')
+
+
+if __name__ == '__main__':
+    hello_two()
---
+++ src/imports-03Nov25/say_hello.py
@@ -0,0 +1,5 @@
+from hello import hello
+from hello_two import hello_two
+
+hello()
+hello_two()
